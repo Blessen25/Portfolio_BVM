bb1f0205cc914ddb1d02c7bca8f4b82bfc79ee36
Blessen25 <blessenvinoymathew25@gmail.com>
2025-08-14 07:21:56 +1000

    Commited Changes

---
+++ frontend/portfolio/src/constants.tsx
@@ -0,0 +1,11 @@
+/* 
+    * This file is part of the Portfolio project.
+    
+
+    * DARK COLOR PALLETE *
+    * BACKGROUND: #0A0A0A
+    * TEXT: # FF2E2E
+    * BUTTON : #FF2E2E WHITE #CC2A2A -> BACKGROUND TEXTCOLOR BORDERCOLOR
+    * HOVERBUTTON : 
+
+*/
