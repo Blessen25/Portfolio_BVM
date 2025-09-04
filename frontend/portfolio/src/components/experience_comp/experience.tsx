c36251a32106f1ff5f78f29fd9a0805e8948ecda
Blessen25 <blessenvinoymathew25@gmail.com>
2025-09-04 21:10:22 +1000

    Commited Changes

--- frontend/portfolio/src/components/experience_comp/experience.tsx
+++ frontend/portfolio/src/components/experience_comp/experience.tsx
@@ -12,7 +12,14 @@ const Experience_Comp:React.FC<HeaderProps> = ({darkMode, setDarkMode}) => {
                 <h1 className={`h1text ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}><AutoTypeCareer /></h1>
                 <p className={`ptext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>The path, milestones, and impact of my work.</p>
                 <div className="experienceparent">
-                    
+                    <div className={`vertical_line ${darkMode ? 'background_primary_red' : 'background_primary_black'}`}>
+                    </div>
+                    <div className={`timeline_star ${darkMode ? 'darkmoderound' : 'lightmoderound'}`}>
+                        <i className={`fa-solid fa-star iconfont`}></i>
+                    </div>
+                    <div className="firstexperiencediv">
+                        
+                    </div>
                 </div>
             </div>
         </>
