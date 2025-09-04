84487ad829c3e0468d30db0962b9d02eb7646b00
Blessen25 <blessenvinoymathew25@gmail.com>
2025-09-04 23:37:27 +1000

    Commited Changes

--- frontend/portfolio/src/components/experience_comp/experience.tsx
+++ frontend/portfolio/src/components/experience_comp/experience.tsx
@@ -1,5 +1,5 @@
 import type { HeaderProps } from '../../interface';
-import { AutoTypeCareer } from '../extras';
+import { AutoTypeCareer, ButtonComp } from '../extras';
 import './experience.css';
 
 const Experience_Comp:React.FC<HeaderProps> = ({darkMode, setDarkMode}) => {
@@ -17,8 +17,20 @@ const Experience_Comp:React.FC<HeaderProps> = ({darkMode, setDarkMode}) => {
                     <div className={`timeline_star ${darkMode ? 'darkmoderound' : 'lightmoderound'}`}>
                         <i className={`fa-solid fa-star iconfont`}></i>
                     </div>
-                    <div className={`firstexperiencediv ${darkMode ? 'darkmodeaboutcard darkmoderound' : 'lightmodeaboutcard lightmoderound'}`}>
+                    <div className={`firstexperiencediv ${darkMode ? 'darkmodeaboutcard darkmodetext' : 'lightmodeaboutcard lightmodetext'}`}>
                         <h1 className='h1smalltext'>Full Stack Developer</h1>
+                        <p className='ptext'>FreeLancer</p>
+                        <p className='ptext'>Freelance full-stack developer delivering end-to-end web apps for small businesses. I ship responsive, accessible UIs with React, HTML/CSS and wire them to secure Django/Python APIs with SQL and Laravel when needed. I handle scoping, development, testing, deployment, and optimizing performance, SEO, and usability.</p>
+                        <div className="experienceskilldiv">
+                            <ButtonComp text="HTML" className={`${darkMode ? 'darkmodetwobutton' : 'lightmodetwobutton'}`}/>
+                            <ButtonComp text="CSS" className={`${darkMode ? 'darkmodetwobutton' : 'lightmodetwobutton'}`}/>
+                            <ButtonComp text="JS" className={`${darkMode ? 'darkmodetwobutton' : 'lightmodetwobutton'}`}/>
+                            <ButtonComp text="React js" className={`${darkMode ? 'darkmodetwobutton' : 'lightmodetwobutton'}`}/>
+                            <ButtonComp text="Django" className={`${darkMode ? 'darkmodetwobutton' : 'lightmodetwobutton'}`}/>
+                            <ButtonComp text="SQL" className={`${darkMode ? 'darkmodetwobutton' : 'lightmodetwobutton'}`}/>
+                            <ButtonComp text="Laravel" className={`${darkMode ? 'darkmodetwobutton' : 'lightmodetwobutton'}`}/>
+
+                        </div>
                     </div>
                 </div>
             </div>
