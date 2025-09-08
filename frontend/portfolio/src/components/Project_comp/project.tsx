6a7fd885cde848ce7c6471b75b85cc3b3f67239b
Blessen25 <blessenvinoymathew25@gmail.com>
2025-09-08 18:13:19 +1000

    Commited Changes

--- frontend/portfolio/src/components/Project_comp/project.tsx
+++ frontend/portfolio/src/components/Project_comp/project.tsx
@@ -2,17 +2,44 @@ import React from "react";
 import './project.css';
 import type { HeaderProps } from "../../interface";
 import { AutoTypeProjects } from "../extras";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const Project_Comp:React.FC<HeaderProps> = ({darkMode, setDarkMode}) => {
-
-    return (
+    const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
+return (
         <>
             <div className='projectdiv'>
                 <h1 className={`h1text ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}><AutoTypeProjects/></h1>
                 <p className={`ptext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>From concept to code: designs built, features shipped, results measured.</p>
-                <div className="project-slider">
-                    
-                </div>
+               <Carousel responsive={responsive}>
+                <div>Item 1</div>
+                <div>Item 2</div>
+                <div>Item 3</div>
+                <div>Item 4</div>
+                <div>Item 5</div>
+                <div>Item 6</div>
+                <div>Item 7</div>
+                <div>Item 8</div>
+               </Carousel>
             </div>
         </>
     )
