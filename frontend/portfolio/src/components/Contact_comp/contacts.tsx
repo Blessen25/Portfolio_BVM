3ae96a10b6d2e2b058c026157aafa12529bb10bf
Blessen25 <blessenvinoymathew25@gmail.com>
2025-09-13 13:00:02 +1000

    Commited Changes

--- frontend/portfolio/src/components/Contact_comp/contacts.tsx
+++ frontend/portfolio/src/components/Contact_comp/contacts.tsx
@@ -12,17 +12,8 @@ const ContactComp:React.FC<HeaderProps> = ({darkMode, setDarkMode}) => {
                 <h1 className={`h1text ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}><AutoTypeContacts/></h1>
                 <p className={`ptext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>Open to roles, collaborations, and consultations.</p>
                 <div className={`contactmaindiv`}>
-                    <div className={`whatsappdiv`}>
-                        <img src={`${darkMode ? '../assets/images/projects/whatsappdark.jpg' : '../assets/images/projects/whatsapplight.jpg'}`} alt="Whatsapp pic" className="whatsappimg"/>
-                        <div className={`whatsappheaderdiv ${darkMode ? 'whatsappdarkheader' : 'whatsapplightheader'}`}>
-                            <div className="whatsappprofileround">
-                                <img src="../assets/images/logo/imagetwo_light.jpg" alt="Whatsapp Profile" />
-                            </div>
-                            <div className="whatsappdetails">
-                                <p className="h1smalltext">Blessen Vinoy Mathew</p>
-                                <p className="whatsapptypicaltext">Typically Replies within 24 hours</p>
-                            </div>
-                        </div>
+                    <div className={`whatsappdiv ${darkMode ? 'darkmodeaboutcard' : 'lightmodeaboutcard'}`} >
+                        
                     </div>
                     <div className={`contactusformdiv ${darkMode ? 'darkmodeaboutcard' : 'lightmodeaboutcard'}`}>
 
