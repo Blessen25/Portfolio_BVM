03164682ce418a0f2ac8988175499d2e243862bc
Blessen25 <blessenvinoymathew25@gmail.com>
2025-09-19 11:55:16 +1000

    Commited changes

--- frontend/portfolio/src/components/footer_comp/footer.tsx
+++ frontend/portfolio/src/components/footer_comp/footer.tsx
@@ -10,8 +10,11 @@ const Footer_Comp: React.FC<HeaderProps> = ({darkMode, setDarkMode}) => {
 
             <div className= {`maincontainer footermaincontainer ${darkMode ? 'darkmode' : 'lightmode'}`}>
                 <div className="childcontainer footerdiv">
-                    <h1 className={`h1mediumtext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>Blessen Vinoy Mathew</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam cum eius rem nesciunt quibusdam quo, unde repellat repellendus tempora odio quas, dolores illum velit, non earum minus molestias labore facilis!</p>
+                    <div className="footerdivdetails">
+                        <h1 className={`h1mediumtext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>Blessen Vinoy Mathew</h1>
+                        <p className={`ptext footertext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam cum eius rem nesciunt quibusdam quo, unde repellat repellendus tempora odio quas, dolores illum velit, non earum minus molestias labore facilis!</p>
+                    </div>
+                    
                 </div>
             </div>
         </>
