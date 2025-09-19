b1dad8345831d639ab83e44eba67120b04854c2e
Blessen25 <blessenvinoymathew25@gmail.com>
2025-09-19 12:20:59 +1000

    Commited changes

--- frontend/portfolio/src/components/footer_comp/footer.tsx
+++ frontend/portfolio/src/components/footer_comp/footer.tsx
@@ -11,10 +11,17 @@ const Footer_Comp: React.FC<HeaderProps> = ({darkMode, setDarkMode}) => {
             <div className= {`maincontainer footermaincontainer ${darkMode ? 'darkmode' : 'lightmode'}`}>
                 <div className="childcontainer footerdiv">
                     <div className="footerdivdetails">
-                        <h1 className={`h1mediumtext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>Blessen Vinoy Mathew</h1>
+                        <h1 className={`h1mediumtext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>BLESSEN VINOY MATHEW</h1>
                         <p className={`ptext footertext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam cum eius rem nesciunt quibusdam quo, unde repellat repellendus tempora odio quas, dolores illum velit, non earum minus molestias labore facilis!</p>
                     </div>
-                    
+                    <div className="footerdivlinks">
+                        <p className={`ptext footertext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`} style={{ textDecoration:'underline', fontWeight:'bold'}} >QUICK LINKS</p>
+                        <a href="#" className={`ptext footerlinktext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>Home</a>
+                        <a href="#" className={`ptext footerlinktext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>About</a>
+                        <a href="#" className={`ptext footerlinktext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>Skills</a>
+                        <a href="#" className={`ptext footerlinktext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>Projects</a>
+                        <a href="#" className={`ptext footerlinktext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>Contact</a>
+                    </div>
                 </div>
             </div>
         </>
