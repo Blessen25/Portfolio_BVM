f9d600dbf7de433d1e7965ca0d8ea4d5cea0518b
Blessen25 <blessenvinoymathew25@gmail.com>
2026-06-23 15:00:32 +0300

    View CV

--- frontend/portfolio/src/components/home_comp/home.tsx
+++ frontend/portfolio/src/components/home_comp/home.tsx
@@ -20,7 +20,7 @@ const HomeComponent:React.FC<HeaderProps> = ({darkMode}) => {
                     <h1 className={`h1text ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>Blessen Vinoy Mathew</h1>
                     <p className={`role-line ptext ${darkMode ? 'darkmodetext' : 'lightmodetext'}`}>I’m a <AutoType /></p>
                     <div className="buttondiv">
-                        <ButtonComp text="Download CV" target="_blank" hrefroute="../assets/files/Blessen_Vinoy_Mathew.pdf" className={`${darkMode ? 'darkmodebutton' : 'lightmodebutton'}`}/>
+                        <ButtonComp text="View CV" target="_blank" hrefroute="../assets/files/Blessen_Vinoy_Mathew.pdf" className={`${darkMode ? 'darkmodebutton' : 'lightmodebutton'}`}/>
                         <ButtonComp text="Contact Info" hrefroute="#contact" className={`${darkMode ? 'darkmodetwobutton' : 'lightmodetwobutton'}`}/>
                     </div>
                     <div className="socialdivhome">
